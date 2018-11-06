037f7f39b0824d7210f0f11cbbb70ce9fcbdd767
Paul van Gent <palkab29@gmail.com>
2018-11-06 15:07:02 +0100

    V1.1
    added reference to technical paper pre-print

--- heartpy/heartpy.py
+++ heartpy/heartpy.py
@@ -1,11 +1,15 @@
 '''Noise-resistant heart rate analysis module for Python
 
-#Reference:
+#References:
 van Gent, P., Farah, H., van Nes, N., & van Arem, B. (2018). 
 Heart Rate Analysis for Human Factors: Development and Validation of an Open Source Toolkit for Noisy Naturalistic Heart Rate Data. 
 In Proceedings of the 6th HUMANIST Conference (pp. 173 to 178)
 
-See also:
+van Gent, P., Farah, H., van Nes, N., & van Arem, B. (2018). 
+Analysing Noisy Driver Physiology Real-Time Using Off-the-Shelf Sensors: Heart Rate Analysis Software from the Taking the Fast Lane Project.
+Manuscript submitted for publication, doi.org/10.13140/RG.2.2.24895.56485
+
+#See also:
 http://www.paulvangent.com/2016/03/15/analyzing-a-discrete-heart-rate-signal-using-python-part-1/
 http://www.paulvangent.com/2016/03/21/analyzing-a-discrete-heart-rate-signal-using-python-part-2/
 http://www.paulvangent.com/2016/03/30/analyzing-a-discrete-heart-rate-signal-using-python-part-3/
