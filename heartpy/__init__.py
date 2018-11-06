b63a77eedb6eaf6ba8a6a958ad751ffdf0374711
Paul van Gent <palkab29@gmail.com>
2018-11-06 10:29:12 +0100

    V1.1
    Added setup.py method

---
+++ heartpy/__init__.py
@@ -0,0 +1,2 @@
+name = "HeartPy"
+from .heartpy import *
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="heartpy",
+    version="1.1",
+    author="Paul van Gent",
+    author_email="info@paulvangent.com",
+    description="Heart Rate Analysis Toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/paulvangentcom/heartrate_analysis_python",
+    packages=["heartpy"],
+    install_requires=["numpy", "scipy", "matplotlib"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+)
