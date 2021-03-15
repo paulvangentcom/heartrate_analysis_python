de75c18ffb0dcd9c1ba7939c995e89735f5f536a
Arritmic <arritmic@gmail.com>
2021-03-15 19:39:56 +0200

    Added solution to return NaN values in the fd measurements when rr_list length is smaller than the smoothing spline degree.

--- heartpy/analysis.py
+++ heartpy/analysis.py
@@ -528,69 +528,87 @@ def calc_fd_measures(method='welch', welch_wsize=240, square_spectrum=False, mea
     datalen = int((len(rr_x) - 1)*resamp_factor)
     rr_x_new = np.linspace(int(rr_x[0]), int(rr_x[-1]), datalen)
 
-    # Added to prevent it crashes when rr_list contains less rr intervals than the degree of the smoothing spline
-    if len(rr_x) <= degree_smoothing_spline:
-        degree_smoothing_spline = degree_smoothing_spline - 1 - (degree_smoothing_spline - len(rr_x))
+    if len(rr_x) > degree_smoothing_spline:
+        interpolation_func = UnivariateSpline(rr_x, rr_list, k=degree_smoothing_spline)
+        rr_interp = interpolation_func(rr_x_new)
 
-    interpolation_func = UnivariateSpline(rr_x, rr_list, k=degree_smoothing_spline)
-    rr_interp = interpolation_func(rr_x_new)
+        # RR-list in units of ms, with the sampling rate at 1 sample per beat
+        dt = np.mean(rr_list) / 1000  # in sec
+        fs = 1 / dt  # about 1.1 Hz; 50 BPM would be 0.83 Hz, just enough to get the
+        # max of the HF band at 0.4 Hz according to Nyquist
+        fs_new = fs * resamp_factor
 
-    # RR-list in units of ms, with the sampling rate at 1 sample per beat
-    dt = np.mean(rr_list)/1000   # in sec
-    fs = 1/dt                    # about 1.1 Hz; 50 BPM would be 0.83 Hz, just enough to get the
-                                 # max of the HF band at 0.4 Hz according to Nyquist
-    fs_new = fs*resamp_factor
+        # compute PSD (one-sided, units of ms^2/Hz)
+        if method == 'fft':
+            frq = np.fft.fftfreq(datalen, d=(1 / fs_new))
+            frq = frq[range(int(datalen / 2))]
+            Y = np.fft.fft(rr_interp) / datalen
+            Y = Y[range(int(datalen / 2))]
+            psd = np.power(Y, 2)
 
-    # compute PSD (one-sided, units of ms^2/Hz)
-    if method == 'fft':
-        frq = np.fft.fftfreq(datalen, d=(1/fs_new))
-        frq = frq[range(int(datalen/2))]
-        Y = np.fft.fft(rr_interp)/datalen
-        Y = Y[range(int(datalen/2))]
-        psd = np.power(Y, 2)
+        elif method == 'periodogram':
+            frq, psd = periodogram(rr_interp, fs=fs_new)
 
-    elif method == 'periodogram':
-        frq, psd = periodogram(rr_interp, fs=fs_new)
+        elif method == 'welch':
+            # nperseg should be based on trade-off btw temporal res and freq res
+            # default is 4 min to get about 9 points in the VLF band
+            nperseg = welch_wsize * fs_new
+            if nperseg >= len(rr_x_new):  # if nperseg is larger than the available data segment
+                nperseg = len(rr_x_new)  # set it to length of data segment to prevent scipy warnings
+                # as user is already informed through the signal length warning
+            frq, psd = welch(rr_interp, fs=fs_new, nperseg=nperseg)
+        else:
+            raise ValueError("specified method incorrect, use 'fft', 'periodogram' or 'welch'")
 
-    elif method == 'welch':
-        # nperseg should be based on trade-off btw temporal res and freq res
-        # default is 4 min to get about 9 points in the VLF band
-        nperseg = welch_wsize*fs_new
-        if nperseg >= len(rr_x_new): # if nperseg is larger than the available data segment
-            nperseg = len(rr_x_new)  # set it to length of data segment to prevent scipy warnings
-                                     # as user is already informed through the signal length warning
-        frq, psd = welch(rr_interp, fs=fs_new, nperseg=nperseg)
+        working_data['frq'] = frq
+        if square_spectrum:
+            working_data['psd'] = np.power(psd, 2)
+        else:
+            working_data['psd'] = psd
+
+        # compute absolute power band measures (units of ms^2)
+        df = frq[1] - frq[0]
+        measures['vlf'] = np.trapz(abs(psd[(frq >= 0.0033) & (frq < 0.04)]), dx=df)
+        measures['lf'] = np.trapz(abs(psd[(frq >= 0.04) & (frq < 0.15)]), dx=df)
+        measures['hf'] = np.trapz(abs(psd[(frq >= 0.15) & (frq < 0.4)]), dx=df)
+        measures['lf/hf'] = measures['lf'] / measures['hf']
+
+        measures['p_total'] = measures['vlf'] + measures['lf'] + measures['hf']
+
+        # compute relative and normalized power measures
+        perc_factor = 100 / measures['p_total']
+        measures['vlf_perc'] = measures['vlf'] * perc_factor
+        measures['lf_perc'] = measures['lf'] * perc_factor
+        measures['hf_perc'] = measures['hf'] * perc_factor
+
+        nu_factor = 100 / (measures['lf'] + measures['hf'])
+        measures['lf_nu'] = measures['lf'] * nu_factor
+        measures['hf_nu'] = measures['hf'] * nu_factor
+
+        working_data['interp_rr_function'] = interpolation_func
+        working_data['interp_rr_linspace'] = rr_x_new
+
     else:
-        raise ValueError("specified method incorrect, use 'fft', 'periodogram' or 'welch'")
+        working_data['frq'] = np.nan
+        working_data['psd'] = np.nan
+        measures['vlf'] = np.nan
+        measures['lf'] = np.nan
+        measures['hf'] = np.nan
+        measures['lf/hf'] = np.nan
 
-    working_data['frq'] = frq
-    if square_spectrum:
-        working_data['psd'] = np.power(psd, 2)
-    else:
-        working_data['psd'] = psd
+        measures['p_total'] = np.nan
 
-    # compute absolute power band measures (units of ms^2)
-    df = frq[1] - frq[0]
-    measures['vlf'] = np.trapz(abs(psd[(frq >= 0.0033) & (frq < 0.04)]), dx=df)
-    measures['lf'] = np.trapz(abs(psd[(frq >= 0.04) & (frq < 0.15)]), dx=df)
-    measures['hf'] = np.trapz(abs(psd[(frq >= 0.15) & (frq < 0.4)]), dx=df)
-    measures['lf/hf'] = measures['lf'] / measures['hf']
+        # compute relative and normalized power measures
+        measures['vlf_perc'] = np.nan
+        measures['lf_perc'] = np.nan
+        measures['hf_perc'] = np.nan
 
-    measures['p_total'] = measures['vlf'] + measures['lf'] + measures['hf']
+        measures['lf_nu'] = np.nan
+        measures['hf_nu'] = np.nan
 
-    # compute relative and normalized power measures
-    perc_factor = 100/measures['p_total']
-    measures['vlf_perc'] = measures['vlf']*perc_factor
-    measures['lf_perc'] = measures['lf']*perc_factor
-    measures['hf_perc'] = measures['hf']*perc_factor
-
-    nu_factor = 100/(measures['lf'] + measures['hf'])
-    measures['lf_nu'] = measures['lf']*nu_factor
-    measures['hf_nu'] = measures['hf']*nu_factor
-
-
-    working_data['interp_rr_function'] = interpolation_func
-    working_data['interp_rr_linspace'] = rr_x_new
+        working_data['interp_rr_function'] = np.nan
+        working_data['interp_rr_linspace'] = rr_x_new
+        # TODO: Warning about degree of the interpolation function.
 
     return working_data, measures
 
